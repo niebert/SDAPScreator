2200dc7e88e1731e8c9059688ad9e344a524b0ce
Bert Niehaus <niehausbert@gmail.com>
2015-11-04 18:11:01 +0100

    Menu added to SDAPS Creator. Zenity is used on Linux for GUI. Please install.

---
+++ frames/autoload.js
@@ -0,0 +1,6 @@
+//alert("AUTOLOAD");
+var vTop = top.document.location.href;
+//alert("vTop="+vTop);
+if (vTop == document.location.href) { 
+	setTimeout("top.document.location.href = '../SDAPScreator.html'",200); 
+};
